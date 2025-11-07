fbe2c24eeef2f6c281f0cddbed2bc3720a93f2a8
Roberto <roberto3154_421@hotmail.com>
2025-11-07 12:41:43 -0300

    Initial Commit

---
+++ MetalDuck/MetalDuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalDuckApp.swift
+//  MetalDuck
+//
+//  Created by Roberto Camargo on 07/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MetalDuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
